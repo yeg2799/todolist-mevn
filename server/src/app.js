0551d3c45d66253f596dd09897c70ed6fe1b729b
yeg2799 <yeg2799@gmail.com>
2022-10-11 00:20:17 +0300

    added env folder

--- server/src/app.js
+++ server/src/app.js
@@ -4,20 +4,21 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 const app = express() // create your express app
+const mongo = require('mongodb')
+const dotenv = require('dotenv') // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 // make app use dependencies
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(cors())
 
-const mongo = require('mongodb')
+
 const MongoClient = mongo.MongoClient
-const uri = 'mongodb+srv://yeg2799:yeg1123581321.@todo.qyihjrr.mongodb.net/?retryWrites=true&w=majority'
 var client;
-var mongoClient = new MongoClient(uri, {  maxPoolSize: 50, 
+var mongoClient = new MongoClient(process.env.DATABASE_URL, {  maxPoolSize: 50, 
     wtimeoutMS: 2500,
     useNewUrlParser: true }) 
 mongoClient.connect((err, db) => { // returns db connection
-    console.log(db);
   if (err != null) {
     console.log(err)
     return
